18416466273ae62ef3c5bbb105e58f6a683a9102
kestutisvirbickas <v.kestutis123@gmail.com>
2021-04-10 21:11:14 +0300

    fixed spacing in recipe details

--- client/src/components/Recipes/RecipeDetails.tsx
+++ client/src/components/Recipes/RecipeDetails.tsx
@@ -39,44 +39,49 @@ const RecipeDetails: React.FC<RecipeDetailsProps> = ({}) => {
   };
 
   return (
-    <section className="section has-text-centered">
+    <section className="section">
       {isModalOpen && (
         <DeleteRecipeModal closeModal={toggleModal} recipeTitle={title} />
       )}
-      <div className="container is-max-desktop">
-        <img
-          src={imgSrc}
-          alt="Recipe Image"
-          style={{ maxWidth: "100%", width: "700px" }}
-        />
-        <div className="is-flex is-flex-direction-row is-justify-content-center block">
-          <button
-            className="button is-primary is-light mr-2"
-            onClick={goToUpdateRecipe}
-          >
-            Update
-          </button>
-          <button className="button is-danger" onClick={toggleModal}>
-            Delete
-          </button>
-        </div>
-        <div className="block">
-          <h2 className="title is-2">{title}</h2>
-          <h4 className="title is-4 has-text-primary">{category}</h4>
-        </div>
-        <div className="block">
-          <h4 className="title is-5">Ingredients</h4>
-          <div
-            className="content"
-            dangerouslySetInnerHTML={{ __html: htmlIngredients }}
-          />
-        </div>
-        <div>
-          <h4 className="title is-5">Instructions</h4>
-          <div
-            className="content"
-            dangerouslySetInnerHTML={{ __html: htmlInstructions }}
-          />
+      <div className="container is-max-desktop ">
+        <div className="is-flex is-flex-direction-column">
+          <div className="is-align-self-center block">
+            <img
+              src={imgSrc}
+              alt="Recipe Image"
+              style={{ maxWidth: "100%", width: "700px" }}
+            />
+          </div>
+
+          <div className="is-flex is-flex-direction-row is-justify-content-center block">
+            <button
+              className="button is-primary is-light mr-2"
+              onClick={goToUpdateRecipe}
+            >
+              Update
+            </button>
+            <button className="button is-danger" onClick={toggleModal}>
+              Delete
+            </button>
+          </div>
+          <div className="block has-text-centered">
+            <h2 className="title is-2">{title}</h2>
+            <h4 className="title is-4 has-text-primary">{category}</h4>
+          </div>
+          <div className="block is-align-self-center has-text-centered">
+            <h4 className="title is-5">Ingredients</h4>
+            <div
+              className="content"
+              dangerouslySetInnerHTML={{ __html: htmlIngredients }}
+            />
+          </div>
+          <div className="block is-align-self-center">
+            <h4 className="title is-5 has-text-centered">Instructions</h4>
+            <div
+              className="content"
+              dangerouslySetInnerHTML={{ __html: htmlInstructions }}
+            />
+          </div>
         </div>
       </div>
     </section>
