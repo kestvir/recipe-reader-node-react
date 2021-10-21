f4a9af0ea65b57052dc32ed810bd4233c8a26f20
kestutisvirbickas <v.kestutis123@gmail.com>
2021-10-21 14:09:52 +0300

    Update validation

--- server/validators/recipes.ts
+++ server/validators/recipes.ts
@@ -3,13 +3,13 @@ import { body } from "express-validator";
 export const addRecipeValidator = [
   body("title").trim().notEmpty().withMessage("Title can not be empty."),
   body("category").notEmpty().withMessage("Category can not be empty."),
-  body("img").notEmpty().withMessage("You have to select an image."),
+  body("img.file").notEmpty().withMessage("You have to select an image."),
   body("ingredients")
     .notEmpty()
-    .withMessage("Ingredient list can not be empty"),
+    .withMessage("Ingredient list can not be empty."),
   body("instructions")
     .notEmpty()
-    .withMessage("Instruction list can not be empty"),
+    .withMessage("Instruction list can not be empty."),
   body("category").custom((value) => {
     if (value !== "entree" && value !== "main" && value !== "desert") {
       throw new Error("Category does not exist");
